8ce36ace765a946b8c40f076d2aca20e3b2eda7f
Akabos <vacheslavuslistyj@gmail.com>
2019-10-02 13:14:25 +0300

    Tests for records api endpoint. W/o DB cleaning mechanism (No SetUps and TeadDowns methods)

---
+++ tests/test_vars.py
@@ -0,0 +1,10 @@
+BASE_URL = 'http://127.0.0.1:8000/'
+RECORDS_URL = f'{BASE_URL}records/'
+LIST_URL = f'{BASE_URL}lists/'
+
+BLANK_FIELD_ERROR = 'This field may not be blank.'
+NULL_FIELD_ERROR = 'This field may not be null.'
+
+REQUIRED_FIELD_ERROR = 'This field is required.'
+
+NOT_EXIST_ERROR = 'Invalid hyperlink - Object does not exist.'
